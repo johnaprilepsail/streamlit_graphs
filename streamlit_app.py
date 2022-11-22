c87c673d60f05b30f9ca7d7a61f6aa736ee0de7d
johndatamininglab <99765221+johndatamininglab@users.noreply.github.com>
2022-11-22 11:57:35 -0500

    theme disabled to avoid error

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 from vega_datasets import data
 
-alt.themes.enable("streamlit")
+#alt.themes.enable("streamlit")
 
 @st.experimental_memo
 def get_data():
